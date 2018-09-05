985badd42f1390d65b90234dd2e1b81860b5ffe1
CityzenKabin <42861341+CityzenKabin@users.noreply.github.com>
2018-09-05 14:39:04 +0545

    Update Week 1.py

--- Week 1.py
+++ Week 1.py
@@ -10,18 +10,40 @@ print(a.swapcase())
     
 # Q. The user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
 
-string = input("Enter the strings:")
-substring = input("Enter the substrings:")
-count = string.count(substring)
-count
+Input: 
+ABCDCDC
+CDC
+
+Output:
+2
+
+#Solution
+
+input = 'ABCDCDC'
+sub ='CDC'
+count = 0
+a = len(input)
+for i in range(0,len(input)):
+   if input[i:i+len(sub)]==sub:
+       count += 1
+
+print(count)
+#end
 
 # Q.Write a class in which its one method accepts a string from console and another method to print the characters that have even indexes.
 
-i = input("Enter the characters:")
-for index in range(len(i)):
-    #checks the even indexes and print it
-    if(index % 2 == 0):
-        print(i[index], end="") 
+class Indexes:
+   def __init__(self):
+       self.string=""
+   def get(self):
+       i = input("Enter the characters:")
+       for index in range(len(i)):
+           #checks the even indexes and print it
+           if(index % 2 == 0):
+               print(i[index], end="") 
+obj = Indexes()
+obj.get()
+
 
 #Q. Write a class in which its one method accepts a string from console and another method to print the characters that have even indexes
 
